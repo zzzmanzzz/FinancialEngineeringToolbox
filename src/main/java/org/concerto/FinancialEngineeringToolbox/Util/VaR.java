2f35600f77f4604aca8ceea810affa958cd092c1
wyatt huang <wyatth@gmail.com>
2020-03-02 03:12:02 +0800

    add empirical VaR and CVaR

--- src/main/java/org/concerto/FinancialEngineeringToolbox/Util/VaR.java
+++ src/main/java/org/concerto/FinancialEngineeringToolbox/Util/VaR.java
@@ -1,16 +1,39 @@
 package org.concerto.FinancialEngineeringToolbox.Util;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.commons.math3.stat.descriptive.rank.Percentile;
 
+import java.util.Arrays;
+
 public class VaR {
-    private static Percentile p = new Percentile();
 
     public static double[] getValueAtRisk(double[] data, double[] confidenceLevel) {
         double[] ret = new double[confidenceLevel.length];
+        Percentile p = new Percentile();
 
         for(int i = 0 ; i < ret.length ; i++) {
             double left =  100 - confidenceLevel[i];
             ret[i] = p.evaluate(data, left);
+        }
+        return  ret;
+    }
+
+    public static double[] getConditionalValueAtRisk(double[] data, double[] confidenceLevel) {
+        double[] ret = new double[confidenceLevel.length];
+        double[] tmp = getValueAtRisk(data, confidenceLevel);
+        DescriptiveStatistics ds = new DescriptiveStatistics();
+        Arrays.sort(data);
+
+        for(int i = 0 ; i < tmp.length ; i++) {
+            for(double j : data) {
+                if(j < tmp[i]) {
+                    ds.addValue(j);
+                } else {
+                    break;
+                }
+            }
+            ret[i] = ds.getMean();
+            ds.clear();
         }
         return  ret;
     }
--- src/test/java/org/concerto/FinancialEngineeringToolbox/Util/VaRTest.java
+++ src/test/java/org/concerto/FinancialEngineeringToolbox/Util/VaRTest.java
@@ -28,4 +28,19 @@ class VaRTest {
         assertEquals(-1.28177, q[5], ConstantForTest.EPSLION); // 90.0%
     }
 
+    @Test
+    void getConditionalValueAtRisk() throws ParameterRangeErrorException {
+        int N = 100000;
+        Generator g  = new NormalizedGaussian(N, 123);
+        final double[] data = g.nextRandomVector();
+        final double[] confidenceLevel ={99.99, 99.90, 99.0, 97.5, 95.0, 90.0};
+        double[] q =  VaR.getConditionalValueAtRisk(data, confidenceLevel);
+
+        assertEquals(-4.04557, q[0], ConstantForTest.EPSLION); // 99.99%
+        assertEquals(-3.35263, q[1], ConstantForTest.EPSLION); // 99.90%
+        assertEquals(-2.66098, q[2], ConstantForTest.EPSLION); // 99.0%
+        assertEquals(-2.32977, q[3], ConstantForTest.EPSLION); // 97.5%
+        assertEquals(-2.05787, q[4], ConstantForTest.EPSLION); // 95.0%
+        assertEquals(-1.75250, q[5], ConstantForTest.EPSLION); // 90.0%
+    }
 }
