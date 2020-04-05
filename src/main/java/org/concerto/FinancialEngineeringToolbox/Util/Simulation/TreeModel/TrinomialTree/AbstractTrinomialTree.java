731d55a1849966ad204dc0e4a66de8bbe99f69c0
wyatt huang <wyatth@gmail.com>
2020-04-06 00:57:08 +0800

    add trinomial tree

--- src/main/java/org/concerto/FinancialEngineeringToolbox/Util/Simulation/TreeModel/TrinomialTree/AbstractTrinomialTree.java
+++ src/main/java/org/concerto/FinancialEngineeringToolbox/Util/Simulation/TreeModel/TrinomialTree/AbstractTrinomialTree.java
@@ -2,6 +2,7 @@ package org.concerto.FinancialEngineeringToolbox.Util.Simulation.TreeModel.Trino
 
 import org.concerto.FinancialEngineeringToolbox.Constant;
 import org.concerto.FinancialEngineeringToolbox.Exception.UndefinedParameterValueException;
+import org.concerto.FinancialEngineeringToolbox.Util.Returns.ExecutionReward;
 import org.concerto.FinancialEngineeringToolbox.Util.Simulation.TreeModel.Tree;
 
 public abstract class AbstractTrinomialTree extends Tree {
@@ -18,6 +19,47 @@ public abstract class AbstractTrinomialTree extends Tree {
 
     @Override
     public double getFairPrice(Constant.OptionType optionType, boolean[] strikeSchedule) throws UndefinedParameterValueException {
-        return 0;
+        int size = 2 * N + 1;
+        double[] St = new double[size];
+        double[] prevC = new double[size];
+        double[] currentC;
+
+        St[N] = S0;
+        for (int i = 0 ; i < N; i++) {
+            St[i] = S0 * Math.pow(D, N - i);
+            St[size - 1 - i] = S0 * Math.pow(U, N - i);
+        }
+
+        for ( int i = 0 ; i < size ; i++ ) {
+            prevC[i]= ExecutionReward.execute(St[i], K, optionType);
+        }
+
+        for (int i = N - 1 ; i > 0 ; i--) {
+            size = 2 * i + 1;
+            St = new double[size];
+            currentC = new double[size];
+
+            St[0] = S0 * Math.pow(D, i);
+            St[i] = S0;
+            for (int z = 0 ; z < i; z++) {
+                St[z] = S0 * Math.pow(D, i - z);
+                St[size - 1 - z] = S0 * Math.pow(U, i - z);
+            }
+
+            for (int j = 0; j < size ; j++) {
+                double tempPrice = discount * (probabilityUp * prevC[j + 2] + probabilityMiddle * prevC[j + 1] + probabilityDown * prevC[j]);
+                double executeReword;
+
+                if(!strikeSchedule[i - 1]) {
+                    currentC[j] = tempPrice;
+                } else {
+                    executeReword = ExecutionReward.execute(St[j], K, optionType);
+                    currentC[j] = Math.max(executeReword, tempPrice);
+                }
+            }
+            prevC = currentC;
+        }
+
+        return (prevC[0] * probabilityDown + prevC[1] * probabilityMiddle + prevC[2] * probabilityUp) * discount;
     }
 }
--- src/test/java/org/concerto/FinancialEngineeringToolbox/Util/Simulation/TreeModel/TrinomialTree/BoyleTest.java
+++ src/test/java/org/concerto/FinancialEngineeringToolbox/Util/Simulation/TreeModel/TrinomialTree/BoyleTest.java
@@ -1,9 +1,14 @@
 package org.concerto.FinancialEngineeringToolbox.Util.Simulation.TreeModel.TrinomialTree;
 
 import org.concerto.FinancialEngineeringToolbox.ConstantForTest;
+import org.concerto.FinancialEngineeringToolbox.Exception.UndefinedParameterValueException;
 import org.concerto.FinancialEngineeringToolbox.Util.Simulation.TreeModel.Tree;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.concerto.FinancialEngineeringToolbox.Constant.OptionType.call;
+import static org.concerto.FinancialEngineeringToolbox.Constant.OptionType.put;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BoyleTest {
@@ -40,6 +45,44 @@ class BoyleTest {
         int N = 1000;
         Tree b = new Boyle(100, 110, 0.3, 0.05, N, 0.002221918);
         assertEquals(0.24972, b.getProbabilityDown(), ConstantForTest.EPSLION);
+    }
+
+    @Test
+    void getBoyleEuropeanCallFairPrice() throws UndefinedParameterValueException {
+        int N = 3;
+        boolean[] canExecute = new boolean[N+1];
+        Arrays.fill(canExecute , false);
+        canExecute[N] = true;
+        Tree b = new Boyle(30, 29, 0.3, 0.05, N, 0.166666667);
+        assertEquals(3.43258, b.getFairPrice(call, canExecute), ConstantForTest.EPSLION);
+    }
+
+    @Test
+    void getBoyleEuropeanPutFairPrice() throws UndefinedParameterValueException {
+        int N = 3;
+        boolean[] canExecute = new boolean[N+1];
+        Arrays.fill(canExecute , false);
+        canExecute[N] = true;
+        Tree b = new Boyle(30, 29, 0.3, 0.05, N, 0.166666667);
+        assertEquals(1.71656, b.getFairPrice(put, canExecute), ConstantForTest.EPSLION);
+    }
+
+    @Test
+    void getLRAmericanCallFairPrice() throws UndefinedParameterValueException {
+        int N = 3;
+        boolean[] canExecute = new boolean[N+1];
+        Arrays.fill(canExecute, true);
+        Tree b = new Boyle(30, 29, 0.3, 0.05, N, 0.166666667);
+        assertEquals( 3.43258, b.getFairPrice(call, canExecute), ConstantForTest.EPSLION);
+    }
+
+    @Test
+    void getLRAmericanPutFairPrice() throws UndefinedParameterValueException {
+        int N = 3;
+        boolean[] canExecute = new boolean[N+1];
+        Arrays.fill(canExecute, true);
+        Tree b = new Boyle(30, 29, 0.3, 0.05, N, 0.166666667);
+        assertEquals( 1.73107, b.getFairPrice(put, canExecute), ConstantForTest.EPSLION);
     }
 
 }
